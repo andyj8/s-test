66edbc2d5e2d318e3cc5a592f5d2c5d4fd2ed80c
Andy Brooks <andyjb@gmail.com>
2014-11-13 18:28:47 +0000

    Add logging to scraper

--- src/Sainsburys/Scraper/Scraper/HttpScraper.php
+++ src/Sainsburys/Scraper/Scraper/HttpScraper.php
@@ -2,14 +2,31 @@
 
 namespace Sainsburys\Scraper\Scraper;
 
+use Psr\Log\LoggerInterface;
+
 class HttpScraper implements Scraper
 {
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function __construct($logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * @param string $location
      * @return int
      */
     public function getPageSize($location)
     {
+        $this->logger->info('Getting size of ' . $location);
+
         $head = array_change_key_case(get_headers($location, true));
         $filesize = $head['content-length'];
 
@@ -22,6 +39,8 @@ class HttpScraper implements Scraper
      */
     public function getPageContent($location)
     {
+        $this->logger->info('Getting content of ' . $location);
+
         return file_get_contents($location);
     }
 
